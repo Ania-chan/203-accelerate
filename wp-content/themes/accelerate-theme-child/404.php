36a944eadc9650e10db4a2f648ce3bfdbe4480eb
Ania-chan <ania.jurago@gmail.com>
2018-08-17 20:36:57 +0900

    added styles for about page footer and 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+ get_header(); ?>
+
+<div id="primary" class="site-content error-page clearfix">
+ <div class="main-content error-page" role="main">
+
+   <div class="map-image">
+     <img src="http://localhost/203-accelerate/wp-content/uploads/2018/08/map.png" alt="The image alt-text" />
+   </div>
+
+   <div class="error-text">
+     <h1 id="error-heading">Whoops, Took a Wrong Turn...</h1>
+     <p id="error-paragraph-1">Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleeding you.</p>
+     <p id="error-paragraph-2">Feel free to take a look around our <span>blog</span> or some of our featured <span>work</span>.</p>
+   </div>
+
+
+ </div><!-- #content -->
+</div><!-- #primary -->
+
+
+
+<?php
+ get_footer();
