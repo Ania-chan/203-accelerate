18ca9e8a5e4763dda634d660323643e94fee4059
Ania-chan <ania.jurago@gmail.com>
2018-08-17 22:23:23 +0900

    added twitter module to front page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -51,15 +51,29 @@ get_header(); ?>
 	<section class="recent-posts">
 	 <div class="site-content">
 	  <div class="blog-post">
-	   <h4>From the Blog</h4>
-	    <?php query_posts('posts_per_page=1'); ?>
-	     <?php while ( have_posts() ) : the_post(); ?>
-	       <h3><?php the_title(); ?></h3>
-	       <?php the_excerpt(); ?>
-	     <?php endwhile; ?>
-	    <?php wp_reset_query(); ?>
+	   	<h4>From the Blog</h4>
+	    	<?php query_posts('posts_per_page=1'); ?>
+	     		<?php while ( have_posts() ) : the_post(); ?>
+	       		<h3><?php the_title(); ?></h3>
+	       		<?php the_excerpt(); ?>
+	     		<?php endwhile; ?>
+	    	<?php wp_reset_query(); ?>
 	   </div>
 	 </div>
 	</section>
+
+	<section class="recent-tweets">
+	 <div class="site-content">
+		 <div class="tweet">
+ 	   	<h4>Recent Tweet</h4>
+		 	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+					<div id="secondary" class="widget-area" role="complementary">
+							<?php dynamic_sidebar( 'sidebar-2' ); ?>
+					</div>
+			<?php endif; ?>
+			<p class="read-more-link"><a href="<?php the_permalink(); ?>">Follow Us &rsaquo;</a></p>
+		</div>
+ </div>
+</section>
 
 <?php get_footer(); ?>
